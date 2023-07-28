e53f36ce349790de09bc0e9a4ad8f7d3aedef469
Antoine Balaine <abalaineaccts@gmail.com>
2023-07-28 06:31:07 -0700

    main: add drum actions

---
+++ main/drum_actions/3stroke.lua
@@ -0,0 +1,14 @@
+local info = debug.getinfo(1, "S")
+
+local internal_root_path = info.source:match(".*AB.main."):sub(2) .. "utilities"
+
+local windows_files = internal_root_path:match("\\$")
+if windows_files then
+  package.path = package.path .. ";" .. internal_root_path .. "\\?.lua"
+else
+  package.path = package.path .. ";" .. internal_root_path .. "/?.lua"
+end
+
+local drums = require("drums")
+
+drums.ras3()
