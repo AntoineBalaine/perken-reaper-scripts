17c9f31cf0813c319f7e6c7d60fecd11f7202254
Antoine Balaine <abalaineaccts@gmail.com>
2023-11-06 15:37:03 -0500

    shortcutManager: switch to using string-key lookup

--- imgui/shortcut_list/shortcutManager.lua
+++ imgui/shortcut_list/shortcutManager.lua
@@ -1,4 +1,3 @@
-reaper.ShowConsoleMsg("hello shortcut")
 --[[
 command id+name
 hover: display shortcut?
@@ -108,16 +107,20 @@ AllAvailableKeys = {
 
 ---@param ctx ImGui_Context
 ---@param actions_list ActionName[]
----@param config_path string
+---@param config_path? string
 local function ShortcutManager(ctx, actions_list, config_path)
   local S = {}
-  ---@alias Shortcut integer|nil
+  ---@alias Shortcut {string: boolean|nil}
   ---@type table<string, Shortcut[]>
   S.actions = {}
   function S:init()
-    for i, action in ipairs(actions_list) do
-      self:Create(action)
+    if actions_list[1] == "quit" then
+      self:Create("quit", { [reaper.ImGui_Key_Escape() .. ""] = true })
     end
+    --[[     for i, action in ipairs(actions_list) do
+      self:Create(action)
+    end ]]
+    return self
   end
 
   --- Upon hitting key in the keyboard, record the shortcut.
@@ -132,12 +135,12 @@ local function ShortcutManager(ctx, actions_list, config_path)
   end
 
   ---check all the keys that have been pressed
-  ---@return Shortcut[] keys_pressed array of key codes, each at the index of the code, i.e. key code 1 is at index 1, key code 2 is at index 2, etc.
+  ---@return Shortcut keys_pressed array of key codes, each at the index of the code, i.e. key code 1 is at index 1, key code 2 is at index 2, etc.
   function S:getKeysPressed()
-    local keysPressed = {}
+    local keysPressed = {} ---@type Shortcut
     for keyName, keyCode in pairs(AllAvailableKeys) do
       if reaper.ImGui_IsKeyPressed(ctx, keyCode) then
-        keysPressed[keyCode] = keyCode
+        keysPressed[keyCode .. ""] = true
       end
     end
     return keysPressed
@@ -145,6 +148,16 @@ local function ShortcutManager(ctx, actions_list, config_path)
 
   ---display ImGui window with list of shortcuts and their mappings
   function S:displayShortcutList()
+    --[[     if not r.ImGui_IsPopupOpen(ctx, "Shortcut List") then
+      r.ImGui_OpenPopup(ctx, "Shortcut List")
+    end
+    local center = { r.ImGui_Viewport_GetCenter(r.ImGui_GetWindowViewport(ctx)) }
+    r.ImGui_SetNextWindowPos(ctx, center[1], center[2], r.ImGui_Cond_Appearing(), 0.5, 0.5)
+    r.ImGui_SetNextWindowSizeConstraints(ctx, 400, 300, 400, 300)
+    if r.ImGui_BeginPopupModal(ctx, "Shortcut List", true, r.ImGui_WindowFlags_TopMost() |  r.ImGui_WindowFlags_NoResize()) then
+      r.ImGui_EndPopup(ctx)
+    end
+    r.ImGui_End(ctx) ]]
   end
 
   function S:lookUp(action)
@@ -158,19 +171,22 @@ local function ShortcutManager(ctx, actions_list, config_path)
     ---check all the keys that have been pressed
     ---iterate them
     ---if one of the keys is not in the shortcut, return false
+    ---reciprocal is also true: if one of the keys of keypressed is not in shortcut, return false
     local keysPressed = self:getKeysPressed()
     local shortcut = self:lookUp(action)
-    for i, key in ipairs(keysPressed) do
-      if not shortcut[key] then
-        return false
-      end
+    if next(keysPressed) == nil or next(shortcut) == nil then return false end
+    for k, _ in pairs(shortcut) do
+      if not keysPressed[k] then return false end
+    end
+    for k, _ in pairs(keysPressed) do
+      if not shortcut[k] then return false end
     end
     return true
   end
 
   ---create Action
   ---@param action string
-  ---@param shortcut? Shortcut[]
+  ---@param shortcut? Shortcut
   function S:Create(action, shortcut)
     -- insert in the action table with value nil.
     -- if user opens the shortcut list, he can update the value
@@ -192,4 +208,8 @@ local function ShortcutManager(ctx, actions_list, config_path)
   function S:Delete(action)
     self.actions[action] = nil
   end
+
+  return S:init()
 end
+
+return ShortcutManager
