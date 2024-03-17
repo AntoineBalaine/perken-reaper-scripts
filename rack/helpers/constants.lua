6da50638e215332cc20848f04ffbb4b5b767fa27
Antoine Balaine <abalaineaccts@gmail.com>
2024-03-17 10:46:43 -0400

    rack: constants: add doc

--- rack/helpers/constants.lua
+++ rack/helpers/constants.lua
@@ -1,3 +1,6 @@
+--[[
+WINDOW_HEIGHT and WINDOW_WIDTH are used as defaults for FX sizes and for the fx separator's height.
+]]
 local constants = {}
 constants.WINDOW_HEIGHT = 240
 constants.WINDOW_WIDTH = 280
