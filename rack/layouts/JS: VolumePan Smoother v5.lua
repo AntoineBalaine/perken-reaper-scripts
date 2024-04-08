c641be749044f695a8eaf113804737d8ef1f32d2
Antoine Balaine <abalaineaccts@gmail.com>
2024-04-08 10:28:38 -0400

    rack: layout: save VolumePanSmoother_v5

---
+++ rack/layouts/JS: VolumePan Smoother v5.lua
@@ -0,0 +1,125 @@
+return {
+  displaySettings = {
+    background = 1330597887,
+    borderColor = 255,
+    buttons_layout = 2,
+    custom_Title = "",
+    title_display = 1,
+    window_width = 110
+  },
+  display_name = "JS: Volume/Pan Smoother v5",
+  display_params = {
+    {
+      details = {
+        display_settings = {
+          color = {
+            circle_color = {
+              active = 842150655,
+              base = 255,
+              hovered = 225
+            },
+            dot_color = {
+              active = 1375728383,
+              base = 535412991,
+              hovered = 30122721
+            },
+            text_color = 4294967295,
+            wiper_color = {
+              active = 855636991,
+              base = 16501247,
+              hovered = 14527457
+            }
+          },
+          flags = 0,
+          radius = 16,
+          type = 2,
+          variant = 7,
+          wiper_start = 0,
+          x = 7.0,
+          y = 0
+        },
+        ident = "0",
+        index = 0,
+        name = "Volume (dB)"
+      },
+      display = true,
+      index = 0,
+      name = "Volume (dB)"
+    },
+    {
+      details = {
+        display_settings = {
+          color = {
+            circle_color = {
+              active = 842150655,
+              base = 255,
+              hovered = 225
+            },
+            dot_color = {
+              active = 1375728383,
+              base = 535412991,
+              hovered = 30122721
+            },
+            text_color = 4294967295,
+            wiper_color = {
+              active = 855636991,
+              base = 16501247,
+              hovered = 14527457
+            }
+          },
+          flags = 0,
+          radius = 14,
+          type = 2,
+          variant = 7,
+          wiper_start = 2,
+          x = 10.0,
+          y = 73.0
+        },
+        ident = "1",
+        index = 1,
+        name = "Pan"
+      },
+      display = true,
+      index = 1,
+      name = "Pan"
+    },
+    {
+      details = {
+        display_settings = {
+          color = {
+            circle_color = {
+              active = 842150655,
+              base = 255,
+              hovered = 225
+            },
+            dot_color = {
+              active = 1375728383,
+              base = 535412991,
+              hovered = 30122721
+            },
+            text_color = 4294967295,
+            wiper_color = {
+              active = 855636991,
+              base = 16501247,
+              hovered = 14527457
+            }
+          },
+          flags = 0,
+          radius = 11,
+          type = 2,
+          variant = 7,
+          wiper_start = 2,
+          x = 15.0,
+          y = 154.0
+        },
+        ident = "2",
+        index = 2,
+        name = "Pan Law (dB)"
+      },
+      display = true,
+      index = 2,
+      name = "Pan Law (dB)"
+    }
+  },
+  name = "JS: Volume/Pan Smoother v5"
+}
