95aeee18ffe7daff65a10712af1af8e2527c6204
Antoine Balaine <abalaineaccts@gmail.com>
2024-04-07 22:51:18 -0400

    rack: layouts: switch saving layouts using serpent instead IniParse

--- rack/helpers/layout_reader.lua
+++ rack/helpers/layout_reader.lua
@@ -1,71 +1,128 @@
 local layout_reader = {}
-local IniParse = require("lib.Iniparse.IniParse")
-
+local serpent = require("lib.serpent")
+local table_helpers = require("helpers.table")
 
 ---@param fx TrackFX
-function layout_reader.save(fx)
-    local mapped = {
-        name = fx.name,
-        display_params = fx.display_params,
-        display_settings = fx.displaySettings
+function layout_reader.stringify(fx)
+    local displaySettings_clone = table_helpers.deepCopy(
+        fx.displaySettings,
+        nil,
+        { "background_disabled", -- don't store these properties
+            "background_offline",
+            "borderColor",
+            "title_Clr",
+            "title_Width",
+            "window_height",
+            "labelButtonStyle",
+            "_grid_color",
+            "_grid_size",
+            "_is_collapsed",
+            "custom_Title",
+            "_selected"
 
-    }
-
-
-    local P = {
-        displaySettings = {
-            background = fx.displaySettings.background,
-            -- background_disabled = fx.displaySettings.background_disabled, -- unused
-            -- background_offline = fx.displaySettings.background_offline, -- unused
-            -- borderColor = fx.displaySettings.borderColor, -- don't store this
-            buttons_layout = fx.displaySettings.buttons_layout,
-            -- title_Clr = fx.displaySettings.title_Clr,
-            -- title_Width = fx.displaySettings.title_Width,
-            title_display = fx.displaySettings.title_display,
-            -- window_height = fx.displaySettings.window_height,
-            window_width = fx.displaySettings.window_width,
-            -- labelButtonStyle = fx.displaySettings.labelButtonStyle, -- don't allow storing this
-        }
-    }
-
+        })
     if fx.displaySettings.custom_Title and fx.displaySettings.custom_Title ~= "" then
-        P.displaySettings.custom_Title = fx.displaySettings.custom_Title
+        displaySettings_clone.custom_Title = fx.displaySettings.custom_Title
     end
-    if fx.displaySettings.decorations then
-        P.decorations = {}
-        for idx, decoration in ipairs(fx.displaySettings.decorations) do
-            local curidx = {
-                [idx .. "x"] = decoration.x,
-                [idx .. "y"] = decoration.y,
-                [idx .. "color"] = decoration.color,
-                [idx .. "height"] = decoration.height,
-                [idx .. "type"] = decoration.type,
-                [idx .. "width"] = decoration.width,
-            }
-            for k, v in pairs(curidx) do
-                P.decorations[k] = v
-            end
+
+    local params_clone = {}
+    for _, param in ipairs(fx.display_params) do
+        -- in the params,
+        -- only store the details and display settings and the header info.
+        -- Don't store the details.
+
+        local param_clone = table_helpers.deepCopy(
+            param,
+            nil,
+            { "display", "details", "_selected", "guid" })
+
+        local display_settings_clone = table_helpers.deepCopy(
+            param.details.display_settings,
+            nil,
+            { "component" })
+        param_clone.display_settings = display_settings_clone
+        table.insert(params_clone, param_clone)
+    end
+
+
+    local fx_mapped = {
+        displaySettings = displaySettings_clone,
+        display_params = params_clone
+    }
+
+    local block = serpent.block(fx_mapped, { comment = false })
+    return block
+end
+
+local os_separator = package.config:sub(1, 1)
+---@param project_directory string
+---@param fx_name string
+local function get_file_path(project_directory, fx_name)
+    return project_directory .. os_separator .. "layouts" .. os_separator .. fx_name .. ".lua"
+end
+
+function layout_reader.save(fx)
+    local block = layout_reader.stringify(fx)
+    reaper.ShowConsoleMsg(block .. "\n")
+    -- local file_path = get_file_path(fx.state.project_directory, fx.name)
+    -- file = io.open(file_path, "w")
+    -- file:write(block)
+    -- file:close()
+end
+
+---@param file_path string
+---@return boolean, table|nil
+function layout_reader.read_layout(file_path)
+    local file = io.open(file_path, "r")
+    local read_table
+    local success = false
+    if file then
+        local content = file:read("*a")
+        success,
+        ---@type table
+        read_table = serpent.load(content)
+        file:close()
+    end
+    return success, read_table
+end
+
+---pulled from serpent.lua,
+---modded to set properties recursively,
+---so we don't overwrite entire objects, only the properties that we've stored.
+---@param a table
+---@param b table
+---@return table
+local function merge(a, b)
+    for k, v in pairs(b) do
+        if a[k] and type(a[k]) == "table" and type(v) == "table" then
+            merge(a[k], v)
+        else
+            a[k] = v
         end
     end
+    return a
+end
 
-    local stringified = IniParse.stringify(P)
-    reaper.ShowConsoleMsg(stringified .. "\n")
-    -- for idx, param in ipairs(fx.display_params) do
-    --     local st = param.details.display_settings
-    --     local St = {
-    --         x = st.x,
-    --         y = st.y,
-    --         color = st.color,
-    --         colors = st.color,
-    --         flags = st.flags,
-    --         height = st.height,
-    --         radius = st.radius,
-    --         type = st.type,
-    --         variant = st.variant,
-    --         width = st.width,
-    --         wiper_start = st.wiper_start
-    --     }
-    -- end
+---@param fx TrackFX
+---@return boolean success
+---@return TrackFX|nil fx
+function layout_reader.read_and_merge(fx)
+    -- find the file path for the current fx name
+    --[[
+    if there is a layouts file,
+    create the list of params,
+    iterate the display settings and the fx params,
+    perform validation on the layout's params
+    and merge them
+    --]]
+    local file_path = get_file_path(fx.state.project_directory, fx.name)
+    local success, read_table = layout_reader.read_layout(file_path)
+    if not success or not read_table then
+        return false, fx
+    else
+        local merged = merge(fx, read_table)
+        return true, merged
+    end
 end
 
 return layout_reader
--- rack/helpers/table.lua
+++ rack/helpers/table.lua
@@ -11,20 +11,72 @@ function table_helpers.namedTableLength(T)
     return count
 end
 
----@param t unknown
-function table_helpers.deepCopy(t)
-    local t_type = type(t)
-    local retval
-    if t_type == 'table' then
-        retval = {}
-        for t, n in next, t, nil do
-            retval[table_helpers.deepCopy(t)] = table_helpers.deepCopy(n)
+-- ---@param t unknown
+-- function table_helpers.deepCopy(t)
+--     local t_type = type(t)
+--     local retval
+--     if t_type == 'table' then
+--         retval = {}
+--         for t, n in next, t, nil do
+--             retval[table_helpers.deepCopy(t)] = table_helpers.deepCopy(n)
+--         end
+--         setmetatable(retval, table_helpers.deepCopy(getmetatable(t)))
+--     else
+--         retval = t
+--     end
+--     return retval
+-- end
+
+---Scythe's deep copy function -- TODO investigate and credit original author
+--- Performs a deep copy of the given table - any tables are recursively
+-- deep-copied to the new table.
+--
+-- To explicitly prevent child tables from being deep-copied, set `.__noRecursion
+-- = true`. This particularly important when working with circular references, as
+-- deep-copying will lead to a stack overflow.
+--
+-- Adapted from: http://lua-users.org/wiki/CopyTable
+---@param t     table
+---@param copies? table
+---@param exclude_keys? string[]
+---@return      table
+table_helpers.deepCopy = function(t, copies, exclude_keys)
+    copies = copies or {}
+
+    local copy
+    if type(t) == "table" then
+        if copies[t] then
+            copy = copies[t]
+        else
+            -- Override so we don't end up working through circular references for
+            -- elements, layers, etc
+            if t.__noRecursion then
+                copy = t
+            else
+                copy = {}
+                for k, v in next, t, nil do
+                    local found = false
+                    if exclude_keys then
+                        for _, exclude_key in ipairs(exclude_keys) do
+                            if k == exclude_key then
+                                found = true
+                            end
+                        end
+                    end
+                    if not found then
+                        copy[table_helpers.deepCopy(k, copies, exclude_keys)] = table_helpers.deepCopy(v, copies,
+                            exclude_keys)
+                    end
+                end
+            end
+
+            copies[t] = copy
+            setmetatable(copy, table_helpers.deepCopy(getmetatable(t), copies))
         end
-        setmetatable(retval, table_helpers.deepCopy(getmetatable(t)))
-    else
-        retval = t
+    else -- number, string, boolean, etc
+        copy = t
     end
-    return retval
+    return copy
 end
 
 ---returns an iterator over the table
